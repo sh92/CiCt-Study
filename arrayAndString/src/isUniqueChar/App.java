41187381723920ceb8c23aedbf42d3422b240b24
sh92 <sanghee.lee1992@gmail.com>
2018-07-26 16:15:58 +0900

    isUnique

--- arrayAndString/src/isUniqueChar/App.java
+++ arrayAndString/src/isUniqueChar/App.java
@@ -4,8 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		String str1 = "ABsdadaa";
 		String str2 = "Abcdefg";
-		System.out.println(isUniqueueChar2(str1));
-		System.out.println(isUniqueueChar2(str2));
+		System.out.println(isUniqueueChar1(str1));
+		System.out.println(isUniqueueChar1(str2));
 	}
 
 	private static boolean isUniqueueChar2(String str) {
@@ -18,6 +18,20 @@ public class App {
 				return false;
 			}
 			char_set[val] = true;
+		}
+		return true;
+	}
+
+	private static boolean isUniqueueChar1(String str) {
+		if (str.length() > 256)
+			return false;
+		int checker = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i) - 'a';
+			if ((checker & (1 << val)) > 0) {
+				return false;
+			}
+			checker |= (1 << val);
 		}
 		return true;
 	}
