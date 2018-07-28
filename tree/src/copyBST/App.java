d584bc0e85adba142c67d2483c79545085397450
sh92 <sanghee.lee1992@gmail.com>
2018-07-28 23:18:17 +0900

    CheckBST witout Array

--- tree/src/copyBST/App.java
+++ tree/src/copyBST/App.java
@@ -13,6 +13,23 @@ public class App {
 		copyBST(root.right, array);
 	}
 
+	public static Integer last_printed = null;
+
+	public static boolean checkBST2(TreeNode root) {
+		if (root == null)
+			return true;
+		if (!checkBST2(root.left))
+			return false;
+		if (last_printed != null && root.data <= last_printed) {
+			return false;
+		}
+		last_printed = root.data;
+
+		if (!checkBST2(root.right))
+			return false;
+		return true;
+	}
+
 	public static boolean checkBST(TreeNode root) {
 		index = 0;
 		int[] array = new int[root.getSize()];
@@ -32,6 +49,14 @@ public class App {
 		TreeNode root2 = new TreeNode(6);
 		root2.right = new TreeNode(2);
 		System.out.println(checkBST(root2));
+
+		TreeNode root3 = new TreeNode(6);
+		root3.left = new TreeNode(2);
+		System.out.println(checkBST2(root3));
+
+		TreeNode root4 = new TreeNode(6);
+		root4.right = new TreeNode(2);
+		System.out.println(checkBST2(root4));
 
 	}
 }
