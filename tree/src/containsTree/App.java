95e1e0372cfd24a547df9815a40148d5cabc549b
sh92 <sanghee.lee1992@gmail.com>
2018-07-30 00:48:02 +0900

    containsTree 2

--- tree/src/containsTree/App.java
+++ tree/src/containsTree/App.java
@@ -1,6 +1,36 @@
 package containsTree;
 
 public class App {
+
+	static boolean containsTree2(TreeNode t1, TreeNode t2) {
+		if (t2 == null)
+			return true;
+		return subTree(t1, t2);
+	}
+
+	private static boolean subTree(TreeNode r1, TreeNode r2) {
+		if (r1 == null) {
+			return false;
+		} else if (r1.data == r2.data && matchTree(r1, r2)) {
+			return true;
+		}
+
+		return subTree(r1.left, r2) || subTree(r1.right, r2);
+	}
+
+	private static boolean matchTree(TreeNode r1, TreeNode r2) {
+
+		if (r1 == null && r2 == null) {
+			return true;
+		} else if (r1 == null || r2 == null) {
+			return false;
+		} else if (r1.data != r2.data) {
+			return false;
+		} else {
+			return matchTree(r1.left, r2.left) && matchTree(r1.right, r2.right);
+		}
+	}
+
 	static boolean containsTree(TreeNode t1, TreeNode t2) {
 		StringBuilder string1 = new StringBuilder();
 		StringBuilder string2 = new StringBuilder();
@@ -29,6 +59,7 @@ public class App {
 		t1.right = new TreeNode(3);
 
 		System.out.println(containsTree(t1, t2));
+		System.out.println(containsTree2(t1, t2));
 
 	}
 }
